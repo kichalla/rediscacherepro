e2573c41b809f10f488696d04b4c6814bb557603
Kiran Challa <kichalla@users.noreply.github.com>
2016-06-07 14:16:53 -0700

    Create Output.cmd

---
+++ RedisRepro/Output.cmd
@@ -0,0 +1,27 @@
+C:\Users\kichalla\Documents\Visual Studio 2015\Projects\RedisRepro\RedisRepro\bin\Release [master]> .\RedisRepro.exe
+Enter any key to exit
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:14:06 PM, Cache get duration 19 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:14:11 PM, Cache get duration 5021 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:14:16 PM, Cache get duration 10022 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:14:21 PM, Cache get duration 15023 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:14:26 PM, Cache get duration 20024 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:14:31 PM, Cache get duration 25026 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:14:36 PM, Cache get duration 30027 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:14:41 PM, Cache get duration 35028 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:14:46 PM, Cache get duration 40015 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:14:51 PM, Cache get duration 45017 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:14:56 PM, Cache get duration 50018 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:15:01 PM, Cache get duration 55019 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:15:06 PM, Cache get duration 60020 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:15:11 PM, Cache get duration 65022 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:15:16 PM, Cache get duration 70023 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:15:21 PM, Cache get duration 75023 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:15:26 PM, Cache get duration 80025 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:15:31 PM, Cache get duration 85026 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:15:36 PM, Cache get duration 90027 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:15:41 PM, Cache get duration 95029 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:15:46 PM, Cache get duration 100030 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:15:51 PM, Cache get duration 105031 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:15:56 PM, Cache get duration 110032 ms. Sleep duration 5001
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:16:01 PM, Cache get duration 115034 ms. Sleep duration 5000
+Value bar. Enter: 6/7/2016 2:14:01 PM, Exit: 6/7/2016 2:16:06 PM, Cache get duration 120035 ms. Sleep duration 5000
